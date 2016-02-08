ec5970c0897a4e823402a29dc0fcca6222110418
Aitzol Naberan <anaberan@codesyntax.com>
2016-02-08 13:13:03 +0100

    Add eurokuserver to the path

--- eurokuserver/manage.py
+++ eurokuserver/manage.py
@@ -4,6 +4,8 @@ import sys
 
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.abspath(os.path.join('..',os.path.dirname(__file__))))
+sys.path.insert(0, os.path.abspath('/srv/data/web/vhosts/default/eurokuserver/'))
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     
