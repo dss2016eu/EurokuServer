f4281fc8ffcdf1d8d31a58946a24224b4221cdb6
Aitzol Naberan <anaberan@codesyntax.com>
2016-02-18 10:49:41 +0100

    Lehen lerroarekin ez sortu galderarik.

--- eurokuserver/control/views.py
+++ eurokuserver/control/views.py
@@ -29,32 +29,34 @@ def add_questions(request):
         except:
             alert_message = u'Erroreren bat dago fitxategian. Ezin automatikoki kargatu'
             return render(request, 'admin/load_csv_questions.html', locals())
-        for row in reader:
-            mandatory = row[:7]
-            optional = row[8:]
-            try:
-                title, correct, incorrect_one, incorrect_two, language, provider, url = mandatory
-            except:
-                alert_message = u'Fitxategiaren zutabe kopurua ez da zuzena'
-                return render(request, 'admin/load_csv_questions.html', locals())
-            q = Question.objects.create(title=title,
-                                        correct_answer=correct,
-                                        incorrect_answer_one=incorrect_one,
-                                        incorrect_answer_two=incorrect_two,
-                                        lang=language,
-                                        provider=provider,
-                                        url=url,
-                                        public=True,
-                                        reviewed=True,)
-            for extra_data in optional:
-                if extra_data.startswith('http'):
-                    photo = get_photo_from_url(url=extra_data)
-                    if photo is not None:
-                        q.photo = photo
-                else:
-                    q.attribution = extra_data
-                q.save()
-            view_message = u'Karga bukatu da'
-        
+        kont = 0
+        for row in reader:    
+            if kont != 0:
+                mandatory = row[:7]
+                optional = row[8:]
+                try:
+                    title, correct, incorrect_one, incorrect_two, language, provider, url = mandatory
+                except:
+                    alert_message = u'Fitxategiaren zutabe kopurua ez da zuzena'
+                    return render(request, 'admin/load_csv_questions.html', locals())
+                q = Question.objects.create(title=title,
+                                            correct_answer=correct,
+                                            incorrect_answer_one=incorrect_one,
+                                            incorrect_answer_two=incorrect_two,
+                                            lang=language,
+                                            provider=provider,
+                                            url=url,
+                                            public=True,
+                                            reviewed=True,)
+                for extra_data in optional:
+                    if extra_data.startswith('http'):
+                        photo = get_photo_from_url(url=extra_data)
+                        if photo is not None:
+                            q.photo = photo
+                    else:
+                        q.attribution = extra_data
+                    q.save()
+                view_message = u'Karga bukatu da. {0} galdera gehitu dira'.format(kont)
+            kont += 1
     return render(request, 'admin/load_csv_questions.html', locals())
         
