25acd1ae3cc53901f05794b47fc0495bfb01e530
Aitzol Naberan <anaberan@codesyntax.com>
2016-03-08 12:35:43 +0100

    Parametroak gehitu komandoari

--- eurokuserver/game/management/commands/load_json_questions.py
+++ eurokuserver/game/management/commands/load_json_questions.py
@@ -59,7 +59,6 @@ def load_photo_from_raw_data(title, tags, slug, image_format, image):
         photo.save()
     except Exception, e:
         import pdb;pdb.set_trace()
-        
         print 'Errorea irudi honekin', title, e
     return photo
 
@@ -68,21 +67,31 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('json_file', nargs='?', type=str)
-        parser.add_argument('-l','--language', nargs='?', type=str)
+        parser.add_argument('-l','--language', dest='language', default='eu', nargs='?', type=str)
+        parser.add_argument('-t','--title', dest='title', default=None, nargs='?', type=str)
+        parser.add_argument('-p','--provider', dest='provider', default=None, nargs='?', type=str)
+        parser.add_argument('-u','--url', dest='url', default=None, nargs='?', type=str)
         
     def handle(self, *args, **options):
         filename = options.get('json_file', None)
         language = options.get('language', 'eu')
+        title = options.get('title', None)
+        provider = options.get('provider', None)
+        url = options.get('url', None)
+        
         with open(filename) as f:
             data = json.loads(f.read())
             for question in data:
-                q = Question.objects.create(title=question.get('title'),
+                title = title or question.get('title')
+                provider = provider or question.get('provider')
+                url = url or question.get('url')
+                q = Question.objects.create(title=title,
                                             correct_answer=question.get('correct'),
                                             incorrect_answer_one=question.get('incorrect_answer_one'),
                                             incorrect_answer_two=question.get('incorrect_answer_two'),
                                             lang=language,
-                                            provider=question.get('provider'),
-                                            url=question.get('url'),
+                                            provider=provider,
+                                            url=url,
                                             attribution=question.get('attribution'),
                                             public=True,
                                             reviewed=True,)                  
