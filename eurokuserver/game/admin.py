9189304dd0d71ec12ac148149bc9e2c2a3b887f1
Aitzol Naberan <anaberan@codesyntax.com>
2016-03-11 10:36:18 +0100

    Publish/Unpublish actions in Question admin

--- eurokuserver/game/admin.py
+++ eurokuserver/game/admin.py
@@ -2,10 +2,23 @@ from django.contrib import admin
 from .models import Question, Game
 # Register your models here.
 
+
 class QuestionAdmin(admin.ModelAdmin):
     date_hierarchy = "added"
     list_display = ('title', 'correct_answer', 'public', 'added')
     list_filter = ('lang', 'provider')
+    actions = ['unpublish', 'publish']
+
+    def unpublish(self, request, queryset):
+        updated = queryset.update(public=False)
+        self.message_user(request, '{} galdera aldatuta'.format(updated))
+    unpublish.short_description = u'Unpublish'
+
+    def publish(self, request, queryset):
+        updated = queryset.update(public=True)
+        self.message_user(request, '{} galdera aldatuta'.format(updated))
+    publish.short_description = u'Publish'
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Game)
 
