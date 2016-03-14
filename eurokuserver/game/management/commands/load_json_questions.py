c3c02b15687c5a25a966684f2a74636743b50f6c
Aitzol Naberan <anaberan@codesyntax.com>
2016-03-14 11:27:30 +0100

    Set base url for images

--- eurokuserver/game/management/commands/load_json_questions.py
+++ eurokuserver/game/management/commands/load_json_questions.py
@@ -15,11 +15,13 @@ from photologue.models import Photo
 
 from eurokuserver.game.models import Question
 
+
 def get_photo_from_url(url='', title='', tags='', format='jpg', slug=''):
     """ """
     if not url:
         return None
-    
+    if not url.startswith('http'):
+        url = 'http://tribual.nr-md.com{0}'.format(url)
     try:
         response = requests.get(url)
     except:
@@ -27,10 +29,12 @@ def get_photo_from_url(url='', title='', tags='', format='jpg', slug=''):
     if not response.status_code == 200:
         return None
     image = response.content
-    N=10
-    title = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
-    slug = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
-                   
+    N = 10
+    title = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for _ in range(N))
+    slug = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                   for _ in range(N))
+
     try:
         photo = load_photo_from_raw_data(title, tags, slug, format, image)
     except Exception, e:
@@ -38,41 +42,68 @@ def get_photo_from_url(url='', title='', tags='', format='jpg', slug=''):
         return None
     return photo
 
+
 def load_photo_from_raw_data(title, tags, slug, image_format, image):
     photo = Photo.objects.filter(title=title)
     if not photo.exists():
         photo = Photo()
         photo.title = title
-        photo.tags=tags
-        photo.slug=slug
+        photo.tags = tags
+        photo.slug = slug
     else:
         photo = photo[0]
     image_t = Image.open(ContentFile(image))
     image_t = image_t.convert("RGB")
-    f=StringIO()
-    image_t.save(f,"JPEG")
+    f = StringIO()
+    image_t.save(f, "JPEG")
     f.seek(0)
 
-    
-    photo.image.save('%s.%s' % (slug,image_format), ContentFile(f.read()))
+    photo.image.save('%s.%s' % (slug, image_format), ContentFile(f.read()))
     try:
         photo.save()
     except Exception, e:
-        import pdb;pdb.set_trace()
+        import pdb
+        pdb.set_trace()
         print 'Errorea irudi honekin', title, e
     return photo
+
 
 class Command(BaseCommand):
     help = " load questions from json file "
 
     def add_arguments(self, parser):
         parser.add_argument('json_file', nargs='?', type=str)
-        parser.add_argument('-l','--language', dest='language', default='eu', nargs='?', type=str)
-        parser.add_argument('-t','--title', dest='title', default=None, nargs='?', type=str)
-        parser.add_argument('-p','--provider', dest='provider', default=None, nargs='?', type=str)
-        parser.add_argument('-u','--url', dest='url', default=None, nargs='?', type=str)
-        parser.add_argument('-m','--max', dest='limit', default=None, nargs='?', type=int)
-        
+        parser.add_argument('-l',
+                            '--language',
+                            dest='language',
+                            default='eu',
+                            nargs='?',
+                            type=str)
+        parser.add_argument('-t',
+                            '--title',
+                            dest='title',
+                            default=None,
+                            nargs='?',
+                            type=str)
+        parser.add_argument('-p',
+                            '--provider',
+                            dest='provider',
+                            default=None,
+                            nargs='?',
+                            type=str)
+        parser.add_argument('-u',
+                            '--url',
+                            dest='url',
+                            default=None,
+                            nargs='?',
+                            type=str)
+        parser.add_argument('-m',
+                            '--max',
+                            dest='limit',
+                            default=None,
+                            nargs='?',
+                            type=int)
+
     def handle(self, *args, **options):
         filename = options.get('json_file', None)
         language = options.get('language', 'eu')
@@ -80,7 +111,7 @@ class Command(BaseCommand):
         pprovider = options.get('provider', None)
         purl = options.get('url', None)
         limit = options.get('limit', None)
-        
+
         with open(filename) as f:
             data = json.loads(f.read())
             if limit is not None:
@@ -89,20 +120,19 @@ class Command(BaseCommand):
                 title = ptitle or question.get('title')
                 provider = pprovider or question.get('provider')
                 url = purl or question.get('url')
-                q = Question.objects.create(title=title,
-                                            correct_answer=question.get('correct'),
-                                            incorrect_answer_one=question.get('incorrect_answer_one'),
-                                            incorrect_answer_two=question.get('incorrect_answer_two'),
-                                            lang=language,
-                                            provider=provider,
-                                            url=url,
-                                            attribution=question.get('attribution'),
-                                            public=True,
-                                            reviewed=True,)                  
+                q = Question.objects.create(
+                    title=title,
+                    correct_answer=question.get('correct'),
+                    incorrect_answer_one=question.get('incorrect_answer_one'),
+                    incorrect_answer_two=question.get('incorrect_answer_two'),
+                    lang=language,
+                    provider=provider,
+                    url=url,
+                    attribution=question.get('attribution'),
+                    public=True,
+                    reviewed=True, )
                 if question.get('photo_url', None) is not None:
                     photo = get_photo_from_url(url=question.get('photo_url'))
                     if photo is not None:
                         q.photo = photo
                         q.save()
-                                               
-                    
